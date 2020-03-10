777a8af99acb434fc3397421560c6c20beb74378
starforce333 <starforce@i.ua>
2020-03-10 20:18:00 +0200

    Init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("May the Force be with you!");
+        System.out.println("     *    ");
+        System.out.println("    **    ");
+        System.out.println("   * *    ");
+        System.out.println("  *  *    ");
+        System.out.println(" *   *    ");
+        System.out.println("     *    ");
+        System.out.println("     *    ");
+        System.out.println("     *    ");
+        System.out.println("     *    ");
+        System.out.println(" * * * * *");
     }
 }
